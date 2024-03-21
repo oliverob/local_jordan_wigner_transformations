eebf37525c6a4b63ee331fa08616bed9b5508878
Oliver O'Brien <oliverobrien111@gmail.com>
2024-03-21 16:39:33 +0000

    modified sysiamge

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,5 +1,6 @@
 using PackageCompiler
-create_sysimage(sysimage_path="./binder/sysimage_path",
+create_sysimage([:Pluto, :PlutoUI, :Graphs, :GraphPlot, :Compose, :QuantumAlgebra, :MetaGraphsNext];
+sysimage_path="./binder/sysimage_path",
                 precompile_execution_file = "warmup.jl",
                 replace_default = true,
                 cpu_target = PackageCompiler.default_app_cpu_target())
