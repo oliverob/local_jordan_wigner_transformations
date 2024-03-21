d727744eab18c8c79027e743ac9f1c655124ecb1
Oliver O'Brien <oliverobrien111@gmail.com>
2024-03-21 16:35:39 +0000

    added sysimage path

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,5 +1,6 @@
 using PackageCompiler
-create_sysimage([:Pluto, :PlutoUI, :Distributions, :StatsPlots];
+create_sysimage(sysimage_path="./binder/sysimage_path",
                 precompile_execution_file = "warmup.jl",
                 replace_default = true,
                 cpu_target = PackageCompiler.default_app_cpu_target())
+
