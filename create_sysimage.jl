49a040df0e5c81fa03d01303932cf6816d010b7d
Oliver O'Brien <oliverobrien111@gmail.com>
2024-03-22 11:59:22 +0000

    Fixed sysimage

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,7 +1,6 @@
 using PackageCompiler
-create_sysimage([:Pluto, :PlutoUI, :Graphs, :GraphPlot, :Compose, :QuantumAlgebra, :MetaGraphsNext];
-sysimage_path="./binder/sysimage_path",
+create_sysimage(["Pluto", "PlutoUI", "Graphs", "GraphPlot", "Compose", "QuantumAlgebra", "MetaGraphsNext"];
+                sysimage_path="sysimage.so",
                 precompile_execution_file = "warmup.jl",
-                replace_default = true,
                 cpu_target = PackageCompiler.default_app_cpu_target())
 
